531ebc9de0e9f740371b66065814f6f1cd3b1c43
Ken Hoff <ken@hoff.tech>
2016-02-15 14:14:34 -0700

    removing comments

--- assignments/assignment1.js
+++ assignments/assignment1.js
@@ -2,17 +2,6 @@ var request = require('request');
 var async = require('async');
 
 module.exports = function(url, cb) {
-	console.log("evaluating assignment 1");
-
-	// request(url, function(error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 		if (body.replace(/\s/g, "") == "<html><body><h1>Hello,World!</h1></body></html>") {
-	// 			console.log("body matches");
-	// 		} else {
-	// 			console.log("body does not match");
-	// 		}
-	// 	}
-	// })
 	if (!url) {
 		return cb("URL not found")
 	}
