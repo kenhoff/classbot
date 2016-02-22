5c555f4fd43c8ef750ab1d97fdbee1d15a8e4a39
Ken Hoff <ken@hoff.tech>
2016-02-22 16:30:18 -0700

    flexbox requirement on a4

--- assignments/assignment4.js
+++ assignments/assignment4.js
@@ -7,6 +7,7 @@ var jsdom = require("jsdom")
 // - an element with a `background-image`
 // - an element with `margin: auto`
 // - an element with `text-align: center`
+// - an element with `display: flex`
 
 
 module.exports = function(url, cb) {
@@ -109,6 +110,34 @@ module.exports = function(url, cb) {
 							elements = window.document.getElementsByTagName('*')
 							for (element of elements) {
 								if (window.getComputedStyle(element)["text-align"] == "center") {
+									this.passed = true
+									return cb(null, this)
+								}
+							}
+							this.passed = false
+							return cb(null, this)
+						}.bind(this)
+					})
+				} else {
+					this.passed = false
+					cb(null, this)
+				}
+			}.bind(this))
+		}
+	}, {
+		description: 'site has an element with `display: flex`',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							elements = window.document.getElementsByTagName('*')
+							for (element of elements) {
+								if (window.getComputedStyle(element)["display"] == "flex") {
 									this.passed = true
 									return cb(null, this)
 								}
