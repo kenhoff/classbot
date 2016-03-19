8d37a5c8309b6118155b7c1b090f4d404a20d698
Ken Hoff <ken@hoff.tech>
2016-03-19 16:18:20 -0600

    a11 complete

--- assignments/assignment11.js
+++ assignments/assignment11.js
@@ -5,62 +5,126 @@ var jsdom = require('jsdom');
 
 module.exports = function(url, cb) {
 	if (!url) {
-		return cb("URL not found")
+		return cb("URL not found");
 	}
 	var tests = [{
 		description: "submitted URL was accessible from the internet",
 		assert: function(url, cb) {
 			request(url, function(error, response) {
 				if (!error && response.statusCode == 200) {
-					this.passed = true
-					cb(null, this)
+					this.passed = true;
+					cb(null, this);
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
 	}, {
-		description: 'console.log("asdf")',
+		description: 'a `<script>` tag in the `<head>` section with some internal JavaScript (e.g. no `src`)',
 		assert: function(url, cb) {
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
-					var virtualConsole = jsdom.createVirtualConsole()
-					virtualConsole.on("log", function (message) {
-						console.log("logged something");
-						console.log(message);
-					})
-
 					jsdom.env(body, {
 						url: url,
 						features: {
 							FetchExternalResources: ["link", "css"]
 						},
-						virtualConsole: virtualConsole,
-						done: function(err) {
-							this.passed = false
-							return cb(null, this)
+						done: function(err, window) {
+							var headTags = window.document.getElementsByTagName('head');
+							if (headTags.length != 0) {
+								var scriptTags = headTags[0].getElementsByTagName("script");
+								for (var scriptTag of scriptTags) {
+									// if scriptTag has no src
+									if (!("src" in scriptTag.attributes)) {
+										this.passed = true;
+										return cb(null, this);
+									}
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
 						}.bind(this)
-					})
+					});
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
-	}]
+	}, {
+		description: 'a `<script>` tag in the `<head>` section that references a valid JavaScript file (e.g. using `src`)',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var headTags = window.document.getElementsByTagName('head');
+							if (headTags.length != 0) {
+								var scriptTags = headTags[0].getElementsByTagName("script");
+								for (var scriptTag of scriptTags) {
+									// if scriptTag has no src
+									if ("src" in scriptTag.attributes) {
+										this.passed = true;
+										return cb(null, this);
+									}
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}, {
+		description: 'a `<script>` tag as the _last element_ of the `<body>`',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var bodyTags = window.document.getElementsByTagName('body');
+							if (bodyTags.length != 0) {
+								if (bodyTags[0].lastElementChild.tagName == "SCRIPT") {
+									this.passed = true;
+									return cb(null, this);
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}];
 
 	async.map(tests, function(test, cb) {
-		test.assert(url, cb)
+		test.assert(url, cb);
 	}, function() {
 		for (var test of tests) {
-			delete test.assert
+			delete test.assert;
 		}
 		var scoreObject = {
 			score: calculateScore(tests),
 			tests: tests,
 			url: url
-		}
-		return cb(null, scoreObject)
-	})
-}
+		};
+		return cb(null, scoreObject);
+	});
+};
