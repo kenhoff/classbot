a6175928faf4e6bc5ec635b082c54346ee967a32
Ken Hoff <ken@hoff.tech>
2016-04-13 16:46:15 -0600

    a17, a18, a19

---
+++ assignments/assignment18.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var async = require('async');
+var calculateScore = require('../calculateScore.js');
+
+module.exports = function(url, cb) {
+	if (!url) {
+		return cb("URL not found");
+	}
+	var tests = [{
+		description: "submitted URL was accessible from the internet",
+		assert: function(url, cb) {
+			request(url, function(error, response) {
+				if (!error && response.statusCode == 200) {
+					this.passed = true;
+					cb(null, this);
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}];
+
+	async.map(tests, function(test, cb) {
+		test.assert(url, cb);
+	}, function() {
+		for (var test of tests) {
+			delete test.assert;
+		}
+		var scoreObject = {
+			score: calculateScore(tests),
+			tests: tests,
+			url: url
+		};
+		return cb(null, scoreObject);
+	});
+};
