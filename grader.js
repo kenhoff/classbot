8681f4e6cd6a0537bda6b81e7f597998d3847eed
Ken Hoff <thekenhoff@gmail.com>
2016-06-05 10:27:49 -0600

    binding to port

--- grader.js
+++ grader.js
@@ -1,3 +1,11 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("listening on", port);
+});
+
 if (process.env.NODE_ENV != "production") {
 	require('dotenv').config();
 }
