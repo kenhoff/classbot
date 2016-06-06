af87039b61bc4e301fc20c8e0155d2dabc49a848
Ken Hoff <thekenhoff@gmail.com>
2016-06-06 17:39:56 -0600

    updating session 0 slides link

--- sessions/0.js
+++ sessions/0.js
@@ -1,6 +1,6 @@
 module.exports = {
 	before_class: ["Bring to class: your laptop"],
-	lecture_slides: "https://docs.google.com/presentation/d/1pH4kAZZJExa6CJfcg57BHnYGYfkh9ptuKqA72CH5vbI/edit",
+	lecture_slides: "https://hoff.tech/classes/frontendwebdev/sessions/0",
 	date: "2016.06.06",
 	id: 0
 };
