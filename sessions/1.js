31625eaf702fc5696dc61d99c3d9ba00ed105bf1
Ken Hoff <thekenhoff@gmail.com>
2016-06-06 17:56:39 -0600

    adding treehouse disclaimer, link to new slides

--- sessions/1.js
+++ sessions/1.js
@@ -1,6 +1,6 @@
 module.exports = {
 	before_class: ["Recommended Watching: Console Foundations", "Recommended Reading: How to host a site on Firebase"],
-	lecture_slides: "https://docs.google.com/presentation/d/14mkdG6J91ioyKKGjV6PT2VhHkEoQTQiP-CVvDSL09v4/edit?usp=sharing",
+	lecture_slides: "https://hoff.tech/classes/frontendwebdev/sessions/1",
 	date: "2016.06.08",
 	id: 1
 };
