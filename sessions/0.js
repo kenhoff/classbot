5ae7079a96485bea4ea099116eb9f6cd04d2016e
Ken Hoff <thekenhoff@gmail.com>
2016-06-08 11:59:31 -0600

    Removing old reading info

--- sessions/0.js
+++ sessions/0.js
@@ -1,5 +1,4 @@
 module.exports = {
-	before_class: ["Bring to class: your laptop"],
 	lecture_slides: "https://hoff.tech/classes/frontendwebdev/sessions/0",
 	date: "2016.06.06",
 	id: 0
--- sessions/1.js
+++ sessions/1.js
@@ -1,5 +1,4 @@
 module.exports = {
-	before_class: ["Recommended Watching: Console Foundations", "Recommended Reading: How to host a site on Firebase"],
 	lecture_slides: "https://hoff.tech/classes/frontendwebdev/sessions/1",
 	date: "2016.06.08",
 	id: 1
