3e7275ec53d7c967c66200338158db9e2be29d04
Ken Hoff <thekenhoff@gmail.com>
2016-06-15 11:47:30 -0600

    linting

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -4,7 +4,7 @@ var calculateScore = require("../calculateScore.js");
 
 var request = require('request');
 var async = require('async');
-var jsdom = require("jsdom")
+var jsdom = require("jsdom");
 	// - an external style sheet <-- ignore this for now
 	// - a `div` with an `id` of `main-content`
 	// - a `div` with a `class` of `profile-section`
@@ -12,26 +12,26 @@ var jsdom = require("jsdom")
 	// - an element with a `display` of `inline` assigned using an *internal* style
 	// - an element with a `display` of `inline-block` assigned using an *internal* style
 
-stylesRequired = ['color', 'background-color', 'font-style', 'font-family', 'font-size', 'width', 'height', ['padding-top', 'padding-bottom', 'padding-left', 'padding-right'], 'border-color', 'border-radius', 'border-width', 'border-style', ['margin-top', 'margin-bottom', 'margin-left', 'margin-right']]
+stylesRequired = ['color', 'background-color', 'font-style', 'font-family', 'font-size', 'width', 'height', ['padding-top', 'padding-bottom', 'padding-left', 'padding-right'], 'border-color', 'border-radius', 'border-width', 'border-style', ['margin-top', 'margin-bottom', 'margin-left', 'margin-right']];
 
 module.exports = function(url, cb) {
 	if (!url) {
-		return cb("URL not found")
+		return cb("URL not found");
 	}
 	tests = [{
 		description: "submitted URL was accessible from the internet",
 		assert: function(url, cb) {
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
-					this.passed = true
-					cb(null, this)
+					this.passed = true;
+					cb(null, this);
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
-	}]
+	}];
 
 	tests.push({
 		description: 'site has an external style sheet (a `link` with `rel="stylesheet"` and an `href` to a valid `.css` file)',
@@ -39,52 +39,52 @@ module.exports = function(url, cb) {
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					jsdom.env(body, function(err, window) {
-						elements = window.document.getElementsByTagName('*')
+						elements = window.document.getElementsByTagName('*');
 						async.detectSeries(elements, function(element, asyncCB) {
 							if (element.tagName == "LINK") {
 								if (element.getAttribute("rel") == "stylesheet") {
 									if (element.getAttribute('href').startsWith("http")) {
-										newUrl = element.getAttribute('href')
+										newUrl = element.getAttribute('href');
 									} else if (element.getAttribute('href').startsWith("/")) {
 										pathArray = url.split('/');
 										protocol = pathArray[0];
 										host = pathArray[2];
 										origin = protocol + '//' + host;
-										newUrl = origin + element.getAttribute('href')
+										newUrl = origin + element.getAttribute('href');
 									} else {
 										pathArray = url.split('/');
-										pathArray.splice(-1, 1)
-										newUrl = pathArray.join('/') + "/" + element.getAttribute('href')
+										pathArray.splice(-1, 1);
+										newUrl = pathArray.join('/') + "/" + element.getAttribute('href');
 									}
 									// console.log(newUrl);
 									request(newUrl, function(error, response) {
 										if (!error && response.statusCode == 200) {
-											asyncCB(true)
+											asyncCB(true);
 										} else {
-											asyncCB(false)
+											asyncCB(false);
 										}
-									})
+									});
 								} else {
-									asyncCB(false)
+									asyncCB(false);
 								}
 							} else {
-								asyncCB(false)
+								asyncCB(false);
 							}
 						}, function(element) {
 							if (element) {
-								this.passed = true
-								return cb(null, this)
+								this.passed = true;
+								return cb(null, this);
 							} else {
-								this.passed = false
-								return cb(null, this)
+								this.passed = false;
+								return cb(null, this);
 							}
-						}.bind(this))
-					}.bind(this))
+						}.bind(this));
+					}.bind(this));
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
 	}, {
 		description: 'site has a `div` with an `id` of `main-content`',
@@ -92,21 +92,21 @@ module.exports = function(url, cb) {
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					jsdom.env(body, function(err, window) {
-						elements = window.document.getElementsByTagName('div')
+						elements = window.document.getElementsByTagName('div');
 						for (element of elements) {
 							if (element.getAttribute("id") == "main-content") {
-								this.passed = true
-								return cb(null, this)
+								this.passed = true;
+								return cb(null, this);
 							}
 						}
-						this.passed = false
-						return cb(null, this)
-					}.bind(this))
+						this.passed = false;
+						return cb(null, this);
+					}.bind(this));
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
 	}, {
 		description: 'site has a `div` with a `class` of `profile-section`',
@@ -114,25 +114,25 @@ module.exports = function(url, cb) {
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					jsdom.env(body, function(err, window) {
-						elements = window.document.getElementsByTagName('div')
+						elements = window.document.getElementsByTagName('div');
 						for (element of elements) {
 							if (element.getAttribute("class") == "profile-section") {
-								this.passed = true
-								return cb(null, this)
+								this.passed = true;
+								return cb(null, this);
 							}
 						}
-						this.passed = false
-						return cb(null, this)
-					}.bind(this))
+						this.passed = false;
+						return cb(null, this);
+					}.bind(this));
 				} else {
-					this.passed = false
-					cb(null, this)
+					this.passed = false;
+					cb(null, this);
 				}
-			}.bind(this))
+			}.bind(this));
 		}
-	})
+	});
 
-	displayTypes = ['block', 'inline', 'inline-block']
+	displayTypes = ['block', 'inline', 'inline-block'];
 
 	for (displayType of displayTypes) {
 		tests.push({
@@ -147,26 +147,26 @@ module.exports = function(url, cb) {
 								FetchExternalResources: ["link", "css"]
 							},
 							done: function(err, window) {
-								elements = window.document.getElementsByTagName('*')
+								elements = window.document.getElementsByTagName('*');
 								for (element of elements) {
 									console.log(element.tagName);
 									console.log(window.getComputedStyle(element).display);
 									if (window.getComputedStyle(element).display == this.displayType) {
-										this.passed = true
-										return cb(null, this)
+										this.passed = true;
+										return cb(null, this);
 									}
 								}
-								this.passed = false
-								return cb(null, this)
+								this.passed = false;
+								return cb(null, this);
 							}.bind(this)
-						})
+						});
 					} else {
-						this.passed = false
-						cb(null, this)
+						this.passed = false;
+						cb(null, this);
 					}
-				}.bind(this))
+				}.bind(this));
 			}
-		})
+		});
 	}
 
 	for (style of stylesRequired) {
@@ -185,48 +185,175 @@ module.exports = function(url, cb) {
 							},
 							done: function(err, window) {
 								// console.log(jsdom.serializeDocument(window.document))
-								elements = window.document.getElementsByTagName('*')
+								elements = window.document.getElementsByTagName('*');
 								for (element of elements) {
 									// if the style is an array (one of the array), do a different check
 									if (Array.isArray(this.style)) {
 										for (testStyle of this.style) {
 											if (window.getComputedStyle(element)[testStyle]) {
-												this.passed = true
-												return cb(null, this)
+												this.passed = true;
+												return cb(null, this);
 											}
 										}
 									} else {
 										// else, do a normal check
 										// check the element's style to see if it's got the style we're testing for
 										if (window.getComputedStyle(element)[this.style]) {
-											this.passed = true
-											return cb(null, this)
+											this.passed = true;
+											return cb(null, this);
 										}
 									}
 								}
-								this.passed = false
-								return cb(null, this)
+								this.passed = false;
+								return cb(null, this);
 							}.bind(this)
-						})
+						});
 					} else {
-						this.passed = false
-						cb(null, this)
+						this.passed = false;
+						cb(null, this);
 					}
-				}.bind(this))
+				}.bind(this));
 			}
-		})
+		});
 	}
 
+
+	// 	backgroundStyles = ['background-position', 'background-size', 'background-image'];
+	//
+	// 	for (backgroundStyle of backgroundStyles) {
+	// 		tests.push({
+	// 			backgroundStyle: backgroundStyle,
+	// 			description: 'site has an element with a `' + backgroundStyle + '`',
+	// 			assert: function(url, cb) {
+	// 				request(url, function(error, response, body) {
+	// 					if (!error && response.statusCode == 200) {
+	// 						jsdom.env(body, {
+	// 							url: url,
+	// 							features: {
+	// 								FetchExternalResources: ["link", "css"]
+	// 							},
+	// 							done: function(err, window) {
+	// 								elements = window.document.getElementsByTagName('*');
+	// 								for (element of elements) {
+	// 									if (this.backgroundStyle in window.getComputedStyle(element)) {
+	// 										this.passed = true;
+	// 										return cb(null, this);
+	// 									}
+	// 								}
+	// 								this.passed = false;
+	// 								return cb(null, this);
+	// 							}.bind(this)
+	// 						});
+	// 					} else {
+	// 						this.passed = false;
+	// 						cb(null, this);
+	// 					}
+	// 				}.bind(this));
+	// 			}
+	// 		});
+	// 	}
+	//
+	// 	tests.push({
+	// 		description: 'site has an element with `margin: auto`',
+	// 		assert: function(url, cb) {
+	// 			request(url, function(error, response, body) {
+	// 				if (!error && response.statusCode == 200) {
+	// 					jsdom.env(body, {
+	// 						url: url,
+	// 						features: {
+	// 							FetchExternalResources: ["link", "css"]
+	// 						},
+	// 						done: function(err, window) {
+	// 							elements = window.document.getElementsByTagName('*');
+	// 							for (element of elements) {
+	// 								if (
+	// 									(window.getComputedStyle(element)["margin-top"] == "auto") &&
+	// 									(window.getComputedStyle(element)["margin-right"] == "auto") &&
+	// 									(window.getComputedStyle(element)["margin-bottom"] == "auto") &&
+	// 									(window.getComputedStyle(element)["margin-left"] == "auto")) {
+	// 									this.passed = true;
+	// 									return cb(null, this);
+	// 								}
+	// 							}
+	// 							this.passed = false;
+	// 							return cb(null, this);
+	// 						}.bind(this)
+	// 					});
+	// 				} else {
+	// 					this.passed = false;
+	// 					cb(null, this);
+	// 				}
+	// 			}.bind(this));
+	// 		}
+	// 	}, {
+	// 		description: 'site has an element with `text-align: center`',
+	// 		assert: function(url, cb) {
+	// 			request(url, function(error, response, body) {
+	// 				if (!error && response.statusCode == 200) {
+	// 					jsdom.env(body, {
+	// 						url: url,
+	// 						features: {
+	// 							FetchExternalResources: ["link", "css"]
+	// 						},
+	// 						done: function(err, window) {
+	// 							elements = window.document.getElementsByTagName('*');
+	// 							for (element of elements) {
+	// 								if (window.getComputedStyle(element)["text-align"] == "center") {
+	// 									this.passed = true;
+	// 									return cb(null, this);
+	// 								}
+	// 							}
+	// 							this.passed = false;
+	// 							return cb(null, this);
+	// 						}.bind(this)
+	// 					});
+	// 				} else {
+	// 					this.passed = false;
+	// 					cb(null, this);
+	// 				}
+	// 			}.bind(this));
+	// 		}
+	// 	}, {
+	// 		description: 'site has an element with `display: flex`',
+	// 		assert: function(url, cb) {
+	// 			request(url, function(error, response, body) {
+	// 				if (!error && response.statusCode == 200) {
+	// 					jsdom.env(body, {
+	// 						url: url,
+	// 						features: {
+	// 							FetchExternalResources: ["link", "css"]
+	// 						},
+	// 						done: function(err, window) {
+	// 							elements = window.document.getElementsByTagName('*');
+	// 							for (element of elements) {
+	// 								if (window.getComputedStyle(element)["display"] == "flex") {
+	// 									this.passed = true;
+	// 									return cb(null, this);
+	// 								}
+	// 							}
+	// 							this.passed = false;
+	// 							return cb(null, this);
+	// 						}.bind(this)
+	// 					});
+	// 				} else {
+	// 					this.passed = false;
+	// 					cb(null, this);
+	// 				}
+	// 			}.bind(this));
+	// 		}
+	// 	});
+
+
 	async.map(tests, function(test, cb) {
-		test.assert(url, cb)
+		test.assert(url, cb);
 	}, function(err, results) {
 		for (test of tests) {
-			delete test.assert
+			delete test.assert;
 		}
 		scoreObject = {
 			score: calculateScore(tests),
 			tests: tests
-		}
-		return cb(null, scoreObject)
-	})
-}
+		};
+		return cb(null, scoreObject);
+	});
+};
