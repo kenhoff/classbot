1916d80843ea2583b643994f22df07cf5796e558
Ken Hoff <thekenhoff@gmail.com>
2016-06-28 20:41:08 -0600

    removing `border-color`

--- assignments/assignment06.js
+++ assignments/assignment06.js
@@ -157,6 +157,118 @@ module.exports = function(url, cb) {
 				}
 			}.bind(this))
 		}
+	}, {
+		description: 'site has the standard Bootstrap CSS from bootstrapcdn.com (a `link` with `href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"`)',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var elements = window.document.getElementsByTagName('link');
+							for (var element of elements) {
+								if (element.getAttribute("href") == "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css") {
+									this.passed = true;
+									return cb(null, this);
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}, {
+		description: 'site has the standard Bootstrap Theme CSS from bootstrapcdn.com (a `link` with `href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"`)',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var elements = window.document.getElementsByTagName('link');
+							for (var element of elements) {
+								if (element.getAttribute("href") == "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css") {
+									this.passed = true;
+									return cb(null, this);
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}, {
+		description: 'site has jQuery from googleapis.com (a `script` with `src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"`)',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var elements = window.document.getElementsByTagName('script');
+							for (var element of elements) {
+								if (element.getAttribute("src") == "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js") {
+									this.passed = true;
+									return cb(null, this);
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
+	}, {
+		description: 'the standard Bootstrap Javascript script from bootstrapcdn.com (a `script` with `src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"`)',
+		assert: function(url, cb) {
+			request(url, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					jsdom.env(body, {
+						url: url,
+						features: {
+							FetchExternalResources: ["link", "css"]
+						},
+						done: function(err, window) {
+							var elements = window.document.getElementsByTagName('script');
+							for (var element of elements) {
+								if (element.getAttribute("src") == "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js") {
+									this.passed = true;
+									return cb(null, this);
+								}
+							}
+							this.passed = false;
+							return cb(null, this);
+						}.bind(this)
+					});
+				} else {
+					this.passed = false;
+					cb(null, this);
+				}
+			}.bind(this));
+		}
 	}]
 
 
@@ -194,6 +306,48 @@ module.exports = function(url, cb) {
 				}.bind(this))
 			}
 		})
+	}
+
+
+
+
+
+
+
+
+	var classes = ["container", "row", "col-xs-", "col-sm-", "col-md-", "col-lg-"];
+
+	for (var bootstrapClass of classes) {
+		tests.push({
+			bootstrapClass: bootstrapClass,
+			description: 'a `div` with class `' + (bootstrapClass.endsWith("-") ? bootstrapClass + "*" : bootstrapClass) + "`",
+			assert: function(url, cb) {
+				request(url, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						jsdom.env(body, {
+							url: url,
+							features: {
+								FetchExternalResources: ["link", "css"]
+							},
+							done: function(err, window) {
+								var elements = window.document.getElementsByTagName('div');
+								for (var element of elements) {
+									if (element.getAttribute("class") && element.getAttribute("class").includes(this.bootstrapClass)) {
+										this.passed = true;
+										return cb(null, this);
+									}
+								}
+								this.passed = false;
+								return cb(null, this);
+							}.bind(this)
+						});
+					} else {
+						this.passed = false;
+						cb(null, this);
+					}
+				}.bind(this));
+			}
+		});
 	}
 
 	async.map(tests, function(test, cb) {
