fd6f085c4de34b11eca66c758a7ad326dd025422
Ken Hoff <thekenhoff@gmail.com>
2016-06-29 17:54:35 -0600

    description 6

--- assignments/assignment06.js
+++ assignments/assignment06.js
@@ -4,47 +4,50 @@ var request = require('request');
 var async = require('async');
 var calculateScore = require('../calculateScore.js');
 
-module.exports = function(url, cb) {
-	if (!url) {
-		return cb("URL not found");
-	}
-	var tests = [{
-		description: "submitted URL was accessible from the internet",
-		assert: function(url, cb) {
-			request(url, function(error, response) {
-				if (!error && response.statusCode == 200) {
+module.exports = {
+	description: "Hopefully you took a look at the readings for this assignment!\n\nThis one's an easy one. Just submit a link to your favorite theme at https://html5up.net/!",
+	test: function(url, cb) {
+		if (!url) {
+			return cb("URL not found");
+		}
+		var tests = [{
+			description: "submitted URL was accessible from the internet",
+			assert: function(url, cb) {
+				request(url, function(error, response) {
+					if (!error && response.statusCode == 200) {
+						this.passed = true;
+						cb(null, this);
+					} else {
+						this.passed = false;
+						cb(null, this);
+					}
+				}.bind(this));
+			}
+		}, {
+			description: "URL should be a link to your favorite theme at <https://html5up.net/>",
+			assert: function(url, cb) {
+				if (url.match(/https:\/\/html5up\.net\/(.+)/)) {
 					this.passed = true;
 					cb(null, this);
 				} else {
 					this.passed = false;
 					cb(null, this);
 				}
-			}.bind(this));
-		}
-	}, {
-		description: "URL should be a link to your favorite theme at <https://html5up.net/>",
-		assert: function(url, cb) {
-			if (url.match(/https:\/\/html5up\.net\/(.+)/)) {
-				this.passed = true;
-				cb(null, this);
-			} else {
-				this.passed = false;
-				cb(null, this);
 			}
-		}
-	}];
+		}];
 
-	async.map(tests, function(test, cb) {
-		test.assert(url, cb);
-	}, function() {
-		for (var test of tests) {
-			delete test.assert;
-		}
-		var scoreObject = {
-			score: calculateScore(tests),
-			tests: tests,
-			url: url
-		};
-		return cb(null, scoreObject);
-	});
+		async.map(tests, function(test, cb) {
+			test.assert(url, cb);
+		}, function() {
+			for (var test of tests) {
+				delete test.assert;
+			}
+			var scoreObject = {
+				score: calculateScore(tests),
+				tests: tests,
+				url: url
+			};
+			return cb(null, scoreObject);
+		});
+	}
 };
