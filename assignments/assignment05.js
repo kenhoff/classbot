b4f9a89414a47fbeff87637452981e2ebb81cffa
Ken Hoff <thekenhoff@gmail.com>
2016-06-29 15:43:00 -0600

    assignment 5

--- assignments/assignment05.js
+++ assignments/assignment05.js
@@ -1,10 +1,8 @@
-// CSS layouts, the `display` style, flexbox & media queries
-
-var calculateScore = require("../calculateScore.js");
+// Client Project Show-and-Tell, Lab session
 
 var request = require('request');
 var async = require('async');
-var jsdom = require("jsdom");
+var calculateScore = require('../calculateScore.js');
 
 module.exports = function(url, cb) {
 	if (!url) {
@@ -25,134 +23,6 @@ module.exports = function(url, cb) {
 		}
 	}];
 
-
-	var displayTypes = ['block', 'inline', 'inline-block'];
-
-	for (var displayType of displayTypes) {
-		tests.push({
-			displayType: displayType,
-			description: 'site has an element with a `display` of `' + displayType + '` assigned',
-			assert: function(url, cb) {
-				request(url, function(error, response, body) {
-					if (!error && response.statusCode == 200) {
-						jsdom.env(body, {
-							url: url,
-							features: {
-								FetchExternalResources: ["link", "css"]
-							},
-							done: function(err, window) {
-								var elements = window.document.getElementsByTagName('*');
-								for (var element of elements) {
-									if (window.getComputedStyle(element).display == this.displayType) {
-										this.passed = true;
-										return cb(null, this);
-									}
-								}
-								this.passed = false;
-								return cb(null, this);
-							}.bind(this)
-						});
-					} else {
-						this.passed = false;
-						cb(null, this);
-					}
-				}.bind(this));
-			}
-		});
-	}
-
-
-	tests.push({
-		description: 'site has an element with `margin: auto`',
-		assert: function(url, cb) {
-			request(url, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					jsdom.env(body, {
-						url: url,
-						features: {
-							FetchExternalResources: ["link", "css"]
-						},
-						done: function(err, window) {
-							var elements = window.document.getElementsByTagName('*');
-							for (var element of elements) {
-								if (
-									(window.getComputedStyle(element)["margin-top"] == "auto") &&
-									(window.getComputedStyle(element)["margin-right"] == "auto") &&
-									(window.getComputedStyle(element)["margin-bottom"] == "auto") &&
-									(window.getComputedStyle(element)["margin-left"] == "auto")) {
-									this.passed = true;
-									return cb(null, this);
-								}
-							}
-							this.passed = false;
-							return cb(null, this);
-						}.bind(this)
-					});
-				} else {
-					this.passed = false;
-					cb(null, this);
-				}
-			}.bind(this));
-		}
-	}, {
-		description: 'site has an element with `text-align: center`',
-		assert: function(url, cb) {
-			request(url, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					jsdom.env(body, {
-						url: url,
-						features: {
-							FetchExternalResources: ["link", "css"]
-						},
-						done: function(err, window) {
-							var elements = window.document.getElementsByTagName('*');
-							for (var element of elements) {
-								if (window.getComputedStyle(element)["text-align"] == "center") {
-									this.passed = true;
-									return cb(null, this);
-								}
-							}
-							this.passed = false;
-							return cb(null, this);
-						}.bind(this)
-					});
-				} else {
-					this.passed = false;
-					cb(null, this);
-				}
-			}.bind(this));
-		}
-	}, {
-		description: 'site has an element with `display: flex`',
-		assert: function(url, cb) {
-			request(url, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					jsdom.env(body, {
-						url: url,
-						features: {
-							FetchExternalResources: ["link", "css"]
-						},
-						done: function(err, window) {
-							var elements = window.document.getElementsByTagName('*');
-							for (var element of elements) {
-								if (window.getComputedStyle(element)["display"] == "flex") {
-									this.passed = true;
-									return cb(null, this);
-								}
-							}
-							this.passed = false;
-							return cb(null, this);
-						}.bind(this)
-					});
-				} else {
-					this.passed = false;
-					cb(null, this);
-				}
-			}.bind(this));
-		}
-	});
-
-
 	async.map(tests, function(test, cb) {
 		test.assert(url, cb);
 	}, function() {
@@ -161,7 +31,8 @@ module.exports = function(url, cb) {
 		}
 		var scoreObject = {
 			score: calculateScore(tests),
-			tests: tests
+			tests: tests,
+			url: url
 		};
 		return cb(null, scoreObject);
 	});
