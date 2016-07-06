10414bd7366ef16bcc8480838aee5e956a5b09ec
Ken Hoff <thekenhoff@gmail.com>
2016-07-06 12:31:01 -0600

    moving dotenv earlier

--- grader.js
+++ grader.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV != "production") {
+	require('dotenv').config();
+}
+
 var express = require('express');
 var app = express();
 var moment = require('moment');
@@ -10,9 +14,6 @@ app.listen(port, function() {
 	console.log("Listening on", port); // eslint-disable-line no-console
 });
 
-if (process.env.NODE_ENV != "production") {
-	require('dotenv').config();
-}
 
 var assignments = require("./assignments");
 var readings = require("./readings");
