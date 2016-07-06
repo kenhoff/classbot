a155fb836d3fdce217bf84f046b49df95f8b1970
Ken Hoff <thekenhoff@gmail.com>
2016-07-06 15:08:29 -0600

    removing build dir, updating slides 7

--- site/metalsmith.js
+++ site/metalsmith.js
@@ -3,9 +3,12 @@ var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var branch = require('metalsmith-branch');
 var htmlToSlides = require('./htmlToSlides.js');
+var changed = require('metalsmith-changed');
 
 console.log("Starting..."); // eslint-disable-line no-console
 Metalsmith(__dirname)
+	.clean(false) // don't destroy `build` dir every time
+	.use(changed()) // only write changed files
 	.source(__dirname + "/src")
 	.destination(__dirname + "/build")
 	.use(markdown())
