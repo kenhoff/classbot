6d6db49710e7844dfab3b1f21e9b7bd7c538ad86
Ken Hoff <thekenhoff@gmail.com>
2016-07-12 12:15:03 -0600

    assignment 9 description

--- assignments/assignment09.js
+++ assignments/assignment09.js
@@ -4,36 +4,39 @@ var request = require('request');
 var async = require('async');
 var calculateScore = require('../calculateScore.js');
 
-module.exports = function(url, cb) {
-	if (!url) {
-		return cb("URL not found")
-	}
-	var tests = [{
-		description: "submitted URL was accessible from the internet",
-		assert: function(url, cb) {
-			request(url, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					this.passed = true
-					cb(null, this)
-				} else {
-					this.passed = false
-					cb(null, this)
-				}
-			}.bind(this))
+module.exports = {
+	description: "Hm. We just kicked off the TSA Randomizer project, so I don't really care what you submit for this assignment!\n\nHow about...you just submit a site that you find technically or visually interesting? Something that we could throw on *More Dumb Fun Web Stuff*. Whatever you want!",
+	test: function(url, cb) {
+		if (!url) {
+			return cb("URL not found");
 		}
-	}]
+		var tests = [{
+			description: "submitted URL was accessible from the internet",
+			assert: function(url, cb) {
+				request(url, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						this.passed = true;
+						cb(null, this);
+					} else {
+						this.passed = false;
+						cb(null, this);
+					}
+				}.bind(this));
+			}
+		}];
 
-	async.map(tests, function(test, cb) {
-		test.assert(url, cb)
-	}, function(err, results) {
-		for (var test of tests) {
-			delete test.assert
-		}
-		var scoreObject = {
-			score: calculateScore(tests),
-			tests: tests,
-			url: url
-		}
-		return cb(null, scoreObject)
-	})
-}
+		async.map(tests, function(test, cb) {
+			test.assert(url, cb);
+		}, function(err, results) {
+			for (var test of tests) {
+				delete test.assert;
+			}
+			var scoreObject = {
+				score: calculateScore(tests),
+				tests: tests,
+				url: url
+			};
+			return cb(null, scoreObject);
+		});
+	}
+};
