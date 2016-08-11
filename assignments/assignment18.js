318c67591304ce13609283bd5bd0e3f071f87663
Ken Hoff <ken@hoff.tech>
2016-08-10 18:47:43 -0600

    tweaking assignment 18

--- assignments/assignment18.js
+++ assignments/assignment18.js
@@ -4,15 +4,19 @@ var request = require('request');
 var async = require('async');
 var calculateScore = require('../calculateScore.js');
 
+var names = ["kirk", "spock", "bones", "uruha", "scotty", "chekov", "sulu"];
+
 module.exports = function(url, cb) {
 	if (!url) {
 		return cb("URL not found");
 	}
+
 	var tests = [{
-		description: "submitted URL was accessible from the internet",
+		description: "server responds to the root URL",
 		assert: function(url, cb) {
 			request(url, function(error, response) {
 				if (!error && response.statusCode == 200) {
+					// console.log(response.body);
 					this.passed = true;
 					cb(null, this);
 				} else {
@@ -22,6 +26,35 @@ module.exports = function(url, cb) {
 			}.bind(this));
 		}
 	}];
+
+	for (var name of names) {
+		tests.push({
+			name: name,
+			description: "a request to `/hello/" + name + "` returns " + '`Hello, ' + name + "!`",
+			assert: function(url, cb) {
+				let testingURL;
+				if (url.endsWith("/")) {
+					testingURL = url + "hello/" + this.name;
+				} else {
+					testingURL = url + "/hello/" + this.name;
+				}
+				request(testingURL, function(error, response) {
+					if (!error && response.statusCode == 200) {
+						if (response.body == "Hello, " + this.name + "!") {
+							this.passed = true;
+							cb(null, this);
+						} else {
+							this.passed = false;
+							cb(null, this);
+						}
+					} else {
+						this.passed = false;
+						cb(null, this);
+					}
+				}.bind(this));
+			}
+		});
+	}
 
 	async.map(tests, function(test, cb) {
 		test.assert(url, cb);
@@ -37,43 +70,3 @@ module.exports = function(url, cb) {
 		return cb(null, scoreObject);
 	});
 };
-
-
-
-// var tests = [{
-// 	description: "server responds to the root URL",
-// 	assert: function(url, cb) {
-// 		request(url, function(error, response) {
-// 			if (!error && response.statusCode == 200) {
-// 				// console.log(response.body);
-// 				this.passed = true;
-// 				cb(null, this);
-// 			} else {
-// 				this.passed = false;
-// 				cb(null, this);
-// 			}
-// 		}.bind(this));
-// 	}
-// }];
-// for (var name of names) {
-// 	tests.push({
-// 		name: name,
-// 		description: "a request to `/hello/" + name + "` returns " + '`Hello, ' + name + "!`",
-// 		assert: function(url, cb) {
-// 			request(url + "/hello/" + this.name, function(error, response) {
-// 				if (!error && response.statusCode == 200) {
-// 					if (response.body == "Hello, " + this.name + "!") {
-// 						this.passed = true;
-// 						cb(null, this);
-// 					} else {
-// 						this.passed = false;
-// 						cb(null, this);
-// 					}
-// 				} else {
-// 					this.passed = false;
-// 					cb(null, this);
-// 				}
-// 			}.bind(this));
-// 		}
-// 	});
-// }
