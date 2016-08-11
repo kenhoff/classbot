f4bd145c15da8d2d18c0c03a9cd1f7685de33fc0
Ken Hoff <ken@hoff.tech>
2016-08-10 22:02:26 -0600

    the last slides :'(

--- assignments/index.js
+++ assignments/index.js
@@ -1,7 +1,7 @@
 var assignments = [];
 
 require("fs").readdirSync(__dirname).forEach(function(file) {
-	if ((file != "index.js") && (file != "tests")) {
+	if ((file != "index.js") && (file != ".DS_Store") && (file != "tests")) {
 		// pull in .mrkdwn file, parse, and build object before pushing onto exports
 		assignments.push(require(__dirname + "/" + file));
 	}
